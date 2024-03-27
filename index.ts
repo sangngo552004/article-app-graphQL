8d0b65acd2ad6ada9dfc41e2f8798e1c4428cfd3
sangngo552004 <sangngo552004@gmail.com>
2024-03-27 08:13:17 +0700

    connect database

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+import {connect as connectDatabase} from "./config/database";
+import Article from "./models/article.model";
+
+dotenv.config();
+connectDatabase();
 
 const app : Express = express();
-const port : Number = 3000;
+const port : Number | String = process.env.PORT || 3000;
 
 //Rest Api
-app.get("/articles", (req : Request, res : Response) => {
+app.get("/articles", async (req : Request, res : Response) => {
+    const articles = await Article.find({
+        deleted : false
+    });
     res.json({
-        articles : []
+        articles : articles
     });
 });
 
